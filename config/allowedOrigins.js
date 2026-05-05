5ca3ba0dc0d91d25888d71f3a63c815c3b915d80
anto254 <jenga.anto@gmail.com>
2026-05-05 14:56:26 +0300

    updated config

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-  "https://fafullz.org",
-  "https://www.fafullz.org",
-  "http://localhost:3000",
-  "http://192.168.100.72:3000",
-  "http://localhost:5173"
+    "https://fafullz.org",
+    "https://www.fafullz.org",
+    "https://fafullz.com",
+    "https://www.fafullz.com",
+    "http://localhost:5173"
 ];
 
 module.exports = allowedOrigins;
